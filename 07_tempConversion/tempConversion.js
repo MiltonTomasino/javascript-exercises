f537f528687274b514d970a3cd704dfe659ea395
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-12 21:41:46 -0700

    Submit Solution to Assignment 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let num = (5/9) * (farenheit - 32);
+
+  return num.toFixed(1) == 0.0 ? 0 : parseFloat(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let num = 32 + (celcius * (9/5));
+
+  return num.toFixed(1) === 0.0 ? 0 : parseFloat(num.toFixed(1));
 };
 
 // Do not edit below this line
