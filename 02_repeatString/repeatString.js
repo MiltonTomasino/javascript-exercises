363abba0c79b5977da8fec69bc46cc1d8042cf8e
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-12 21:39:36 -0700

    Submit Solution to Assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, numOfTimes) {
+    if (numOfTimes < 0) return "ERROR";
+    
+    let res = "";
 
+    for (let i = 0; i < numOfTimes; i++) {
+        res += str;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
