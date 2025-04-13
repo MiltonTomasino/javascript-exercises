f7bd270eec9b2b17b67030e82be09cefa3562501
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-12 21:39:53 -0700

    Submit Solution to Assignment 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let res = ""
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        res += str[i];
+    }
+
+    return res;
+
+    // return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
